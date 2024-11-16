4204ba220066d21b7044a795781c74686ac8b6c2
alice bergonhe <alicebergonhe@air-de-alice.home>
2024-11-16 21:05:44 +0100

    add roboto font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'Roboto': '"Roboto Mono", monospace',
+      },
+      blur: {
+        xs: '0.5px',
+      }
+    },
   },
   plugins: [],
 }

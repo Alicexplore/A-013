6f566f891d26e00cd106c5b64c4550c970163c2c
alice bergonhe <alicebergonhe@air-de-alice.home>
2024-12-28 20:46:34 +0100

    create Text infos components for index page

---
+++ src/components/TextFolio.jsx
@@ -0,0 +1,29 @@
+import { motion } from "framer-motion";
+
+const TextFolio = () => {
+  return (
+    <div
+          className="absolute bottom-5 right-5 md:bottom-5 md:right-10 font-Roboto text-[9px] sm:text-[10.5px] md:text-xs tracking-widest 
+        text-[#222222] mb-0 sm:mb-5 sm:text-left pointer-events-none">
+          <h1>A-013 / V1.0.0</h1>
+          <div className="flex items-baseline">
+            <p>FOLIO 2025</p>
+            <motion.div
+              className="right-[2px] w-[7px] h-[9px] bg-[#222222] ml-[1px] mt-[3px]"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: [0, 1, 1, 0] }}
+              transition={{
+                opacity: {
+                  repeat: Infinity,
+                  repeatType: "loop",
+                  duration: 0.7,
+                  ease: "easeInOut",
+                },
+              }}
+            ></motion.div>
+          </div>
+        </div>
+  )
+}
+
+export default TextFolio
---
+++ src/components/TextName.jsx
@@ -0,0 +1,27 @@
+import ScrambleText from "./ScrambleText"
+
+const TextName = () => {
+  return (
+    <div className="absolute top-5 left-5 sm:top-10 sm:left-10 flex flex-col font-Roboto text-[9px] sm:text-[10.5px] md:text-xs 
+    tracking-widest text-[#222] z-40">
+        <div className="pointer-events-none mix-blend-difference">
+            <h1>ALICE BERGONHE</h1>
+            <h1>FRONTEND DEVELOPER</h1>
+        </div>
+        <div className="mt-3 sm:mt-4">
+            <ScrambleText
+              className="text-xs"
+              text="/ LINKEDIN"
+              scrambleDuration={1.5}
+            />
+            <ScrambleText
+              className="text-xs"
+              text="/ GITHUB"
+              scrambleDuration={1.5}
+            />
+        </div>
+    </div> 
+  )
+}
+
+export default TextName

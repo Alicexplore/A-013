f7481851b33998b48fb5722d6e23955d847de067
alice bergonhe <alicebergonhe@air-de-alice.home>
2024-12-28 20:52:50 +0100

    Create MediaCards to use as links for social medias

---
+++ src/components/MediaCards.jsx
@@ -0,0 +1,100 @@
+import { motion } from "framer-motion";
+
+const MediaCards = () => {
+  return (
+    <div className="absolute flex flex-row md:flex-col items-start md:items-center justify-between -bottom-[105px] md:bottom-auto 
+    left-8 md:-left-[150px] gap-[35px] md:gap-[50px]">
+    
+    <div className="rotate-[-20deg]">
+    <motion.div
+      className="relative w-[45px] h-[55px] md:w-[60px] md:h-[75px] bg-[#D4D4CE] rounded-[3px] md:rounded-[4px] cursor-pointer"
+      initial={{
+        scale: 1,
+        boxShadow: "2px 2px 8px #252420, inset 1px 1px 2px #fff, inset -2px -2px 2px #9e9e9e",
+      }}
+      whileHover={{
+        scale: 1.1,
+        boxShadow: "6px 6px 15px #252420, inset 1px 1px 3px #fff, inset -3px -3px 3px #9e9e9e",
+      }}
+      transition={{
+        duration: 0.6, 
+        ease: "easeInOut", 
+      }}>
+        <div className="absolute inset-[1px] md:inset-[2px] border-[#fff] border-l-[0.5px] border-t-[0.5px] 
+        rounded-[2px] md:rounded-[3px] blur-xxs md:blur-xs"></div>
+        <div className="absolute inset-[3px] md:inset-[5px] border-[#fff] border-[0.5px] rounded-[1px]"></div>
+
+        <div className="absolute w-full h-full grid grid-rows-2">
+            <div className="row-span-1 flex items-center justify-center">
+                <div className="w-2/3 h-full bg-[#fff] flex items-end justify-center rounded-b-[2px] md:rounded-b-[4px]
+                shadow-[inset_0_3px_3px_#fff,1px_1px_2px_#757575]">
+                    <span className="font-Roboto tracking-widest text-[4px] md:text-[6px] text-[#222] uppercase mb-1">linkedin</span> 
+                </div>
+            </div>
+            <div className="row-span-1 flex items-end justify-center">
+                <div className="h-3/4 w-full flex items-end justify-center">
+                <div className="h-full w-5/6 flex items-end justify-between px-[5px] md:px-[6px] bg-[#2C2B18] rounded-t-sm
+                shadow-[inset_0px_-3px_3px_#000]">
+                      {Array.from({ length: 5 }, (_, index) => (
+                        <span 
+                          key={index}
+                          className="w-[3px] md:w-1 h-3/4"
+                          style={{ background: 'conic-gradient(from 135deg, #b2a9a9, #b2a8a8, #ffffff, #d7dbd9, #ffffff, #b2a8a8)'}}>
+                        </span>
+                      ))} 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </motion.div>
+    </div>
+
+    <div className="rotate-[10deg]">
+    <motion.div
+      className="relative w-[45px] h-[55px] md:w-[60px] md:h-[75px] bg-[#D4D4CE] rounded-[3px] md:rounded-[4px] cursor-pointer"
+      initial={{
+        scale: 1,
+        boxShadow: "2px 2px 8px #252420, inset 1px 1px 2px #fff, inset -2px -2px 2px #9e9e9e",
+      }}
+      whileHover={{
+        scale: 1.1,
+        boxShadow: "6px 6px 15px #252420, inset 1px 1px 3px #fff, inset -3px -3px 3px #9e9e9e",
+      }}
+      transition={{
+        duration: 0.6, 
+        ease: "easeInOut", 
+      }}>
+        <div className="absolute inset-[1px] md:inset-[2px] border-[#fff] border-l-[0.5px] border-t-[0.5px] 
+        rounded-[2px] md:rounded-[3px] blur-xxs md:blur-xs"></div>
+        <div className="absolute inset-[3px] md:inset-[5px] border-[#fff] border-[0.5px] rounded-[1px]"></div>
+
+        <div className="absolute w-full h-full grid grid-rows-2">
+            <div className="row-span-1 flex items-center justify-center">
+                <div className="w-2/3 h-full bg-[#fff] flex items-end justify-center rounded-b-[2px] md:rounded-b-[4px]
+                shadow-[inset_0_3px_3px_#fff,1px_1px_2px_#757575]">
+                    <span className="font-Roboto tracking-widest text-[4px] md:text-[6px] text-[#222] uppercase mb-1">github</span> 
+                </div>
+            </div>
+            <div className="row-span-1 flex items-end justify-center">
+                <div className="h-3/4 w-full flex items-end justify-center">
+                <div className="h-full w-5/6 flex items-end justify-between px-[5px] md:px-[6px] bg-[#2C2B18] rounded-t-sm
+                shadow-[inset_0px_-3px_3px_#000]">
+                      {Array.from({ length: 5 }, (_, index) => (
+                        <span 
+                          key={index}
+                          className="w-[3px] md:w-1 h-3/4"
+                          style={{ background: 'conic-gradient(from 135deg, #b2a9a9, #b2a8a8, #ffffff, #d7dbd9, #ffffff, #b2a8a8)'}}>
+                        </span>
+                      ))} 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </motion.div>
+    </div>
+
+    </div>
+  )
+}
+
+export default MediaCards

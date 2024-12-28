be515230269563d4272490ddd89a28b27b57c6ae
alice bergonhe <alicebergonhe@air-de-alice.home>
2024-12-28 20:53:21 +0100

    Create MediallicNails component to use on the desk

---
+++ src/components/MetallicNails.jsx
@@ -0,0 +1,28 @@
+const MetallicNails = () => {
+  return (
+    <div className="absolute inset-0 w-full h-full z-9">
+     
+
+      <div className="absolute top-0 right-0 h-[22px] w-[22px] bg-[#222] md:h-7 md:w-7 rounded-full mt-4 mr-4 md:mt-8 md:ml-8 
+      shadow-[3px_3px_6px_#222,inset_-2px_-2px_1px_#000]">
+       
+      </div>
+
+      <div className="absolute bottom-0 left-0 h-[22px] w-[22px] bg-[#222] md:h-7 md:w-7 rounded-full mb-4 ml-4 md:mt-8 md:ml-8 
+      shadow-[3px_3px_6px_#222,inset_1px_1px_1px_#7b7b7b,inset_-2px_-2px_1px_#000]">
+        <div className="absolute inset-0 bg-[#222] rounded-full"></div>
+      </div>
+
+   
+    </div> 
+  )
+}
+
+export default MetallicNails
+
+{/* <div className="absolute bottom-0 right-0 h-[22px] w-[22px] md:h-7 md:w-7 rounded-full mb-4 mr-4 md:mt-8 md:ml-8 
+shadow-[3px_3px_6px_#222,inset_-1px_-1px_1px_#7b7b7b]"
+style={{ background: 'conic-gradient(from 135deg, #b2a9a9, #b2a8a8, #ffffff, #d7dbd9, #ffffff, #b2a8a8)'}}>
+  <div className="absolute inset-0 bg-[#222] rounded-full"></div>
+</div>
+</div>  */}

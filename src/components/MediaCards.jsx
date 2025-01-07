8054842c2953611c015c430471752fadfeb845ad
alice bergonhe <alicebergonhe@air-de-alice.home>
2025-01-07 21:32:00 +0100

    Update colors of media cards for dark mode

--- src/components/MediaCards.jsx
+++ src/components/MediaCards.jsx
@@ -7,7 +7,7 @@ const MediaCards = () => {
     
     <div className="rotate-[-20deg]">
     <motion.div
-      className="relative w-[45px] h-[55px] md:w-[60px] md:h-[75px] bg-[#222] rounded-[3px] md:rounded-[4px] cursor-pointer"
+      className="relative w-[45px] h-[55px] md:w-[60px] md:h-[75px] bg-[#2e2e2e] rounded-[3px] md:rounded-[4px] cursor-pointer"
       initial={{
         scale: 1,
         boxShadow: "2px 2px 8px #000, inset 1px 1px 2px #3b3b3b, inset -2px -2px 2px #131313",
@@ -20,9 +20,9 @@ const MediaCards = () => {
         duration: 0.6, 
         ease: "easeInOut", 
       }}>
-        <div className="absolute inset-[1px] md:inset-[2px] border-[#fff]/80 border-l-[0.5px] border-t-[0.5px] 
+        <div className="absolute inset-[1px] md:inset-[2px] border-[#fff]/40 border-l-[0.5px] border-t-[0.5px] 
         rounded-[2px] md:rounded-[3px] blur-xxs md:blur-xs"></div>
-        <div className="absolute inset-[3px] md:inset-[5px] border-[#717171] border-[0.5px] rounded-[1px]"></div>
+        {/* <div className="absolute inset\-[3px] md:inset-[5px] border-[#4b4b4b] border-[0.5px] rounded-[1px] md:rounded-[2px]"></div> */}
 
         <div className="absolute w-full h-full grid grid-rows-2">
             <div className="row-span-1 flex items-center justify-center">
@@ -33,8 +33,8 @@ const MediaCards = () => {
             </div>
             <div className="row-span-1 flex items-end justify-center">
                 <div className="h-3/4 w-full flex items-end justify-center">
-                <div className="h-full w-5/6 flex items-end justify-between px-[5px] md:px-[6px] bg-[#000] rounded-t-sm
-                shadow-[inset_0px_-3px_3px_#000]">
+                <div className="h-full w-5/6 flex items-end justify-between px-[5px] md:px-[6px] bg-[#000] border-[#3b3b3b] border-t-[0.5px]
+                border-l-[0.5px] border-r-[0.5px] rounded-t-sm shadow-[inset_0px_-3px_3px_#000]">
                       {Array.from({ length: 5 }, (_, index) => (
                         <span 
                           key={index}
@@ -51,7 +51,7 @@ const MediaCards = () => {
 
     <div className="rotate-[10deg]">
     <motion.div
-      className="relative w-[45px] h-[55px] md:w-[60px] md:h-[75px] bg-[#222] rounded-[3px] md:rounded-[4px] cursor-pointer"
+      className="relative w-[45px] h-[55px] md:w-[60px] md:h-[75px] bg-[#2e2e2e] rounded-[3px] md:rounded-[4px] cursor-pointer"
       initial={{
         scale: 1,
         boxShadow: "2px 2px 8px #000, inset 1px 1px 2px #3b3b3b, inset -2px -2px 2px #131313",
@@ -64,9 +64,9 @@ const MediaCards = () => {
         duration: 0.6, 
         ease: "easeInOut", 
       }}>
-        <div className="absolute inset-[1px] md:inset-[2px] border-[#fff]/80 border-l-[0.5px] border-t-[0.5px] 
+        <div className="absolute inset-[1px] md:inset-[2px] border-[#fff]/40 border-l-[0.5px] border-t-[0.5px] 
         rounded-[2px] md:rounded-[3px] blur-xxs md:blur-xs"></div>
-        <div className="absolute inset-[3px] md:inset-[5px] border-[#717171] border-[0.5px] rounded-[1px]"></div>
+        {/* <div className="absolute inset\-[3px] md:inset-[5px] border-[#4b4b4b] border-[0.5px] rounded-[1px] md:rounded-[2px]"></div> */}
 
         <div className="absolute w-full h-full grid grid-rows-2">
             <div className="row-span-1 flex items-center justify-center">
@@ -77,8 +77,8 @@ const MediaCards = () => {
             </div>
             <div className="row-span-1 flex items-end justify-center">
                 <div className="h-3/4 w-full flex items-end justify-center">
-                <div className="h-full w-5/6 flex items-end justify-between px-[5px] md:px-[6px] bg-[#000] rounded-t-sm
-                shadow-[inset_0px_-3px_3px_#000]">
+                <div className="h-full w-5/6 flex items-end justify-between px-[5px] md:px-[6px] bg-[#000] border-[#3b3b3b] border-t-[0.5px]
+                border-l-[0.5px] border-r-[0.5px] rounded-t-sm shadow-[inset_0px_-3px_3px_#000]">
                       {Array.from({ length: 5 }, (_, index) => (
                         <span 
                           key={index}

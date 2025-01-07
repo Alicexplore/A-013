d5567be35a60bf9b6145ef14b578e35914009bcd
alice bergonhe <alicebergonhe@air-de-alice.home>
2025-01-07 21:29:09 +0100

    Add Pressed Logic to the buttons

--- src/components/ButtonsDirections.jsx
+++ src/components/ButtonsDirections.jsx
@@ -2,11 +2,17 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 
 const ButtonsDirections = () => {
-  const [extraPressed, setExtraPressed] = useState(false);
+  const [upPressed, setUpPressed] = useState(false);
+  const [downPressed, setDownPressed] = useState(false);
 
-  const handleClick = () => {
-    setExtraPressed(true);
-    setTimeout(() => setExtraPressed(false), 200);
+  const handleUpClick = () => {
+    setUpPressed(true);
+    setTimeout(() => setUpPressed(false), 200); 
+  };
+
+  const handleDownClick = () => {
+    setDownPressed(true);
+    setTimeout(() => setDownPressed(false), 200);
   };
 
   return (
@@ -14,23 +20,26 @@ const ButtonsDirections = () => {
       <div className="w-full h-full relative flex items-center bg-[#000] border-[0.5px] md:border-[1px] border-[#000]">
         <div className="absolute inset-[1px] bg-[#000] rounded-sm">
           <div className="grid grid-rows-3 h-full w-full gap-[1.5px] md:gap-[2px] bg-[#000]">
+           
             <motion.div
-              className="col-span-1 rounded-[4px] md:rounded-md bg-[#d4d4cf] z-2 flex items-center justify-center 
-              text-lg md:text-2xl text-[#4a4840] cursor-pointer"
-              onClick={handleClick}
+              className="col-span-1 rounded-[4px] md:rounded-md bg-[#222] z-2 flex items-center justify-center 
+              text-lg md:text-2xl text-[#d4d4ce] cursor-pointer"
+              onClick={handleUpClick}
               animate={{
-                boxShadow: extraPressed
-                  ? "inset 2px 2px 5px #222, inset -2px -2px 5px #222"
-                  : "inset 2px 2px 1px #fff, inset -2px -2px 1px #979797, 5px 5px 8px #000",
+                boxShadow: upPressed
+                  ? "inset 2px 2px 5px #111, inset -2px -2px 5px #111"
+                  : "inset 2px 2px 1px #3a3a3a, inset -2px -2px 1px #131313, 5px 5px 8px #000",
               }}
               transition={{ duration: 0.1, ease: "easeInOut" }}
             >
               <ion-icon name="caret-up-outline"></ion-icon>
             </motion.div>
-
+           
             <div className="col-span-1 bg-[#222]">
-                <div className="flex justify-between h-full w-full">
-                  {Array(7).fill(null).map((_, index) => (
+              <div className="flex justify-between h-full w-full">
+                {Array(7)
+                  .fill(null)
+                  .map((_, index) => (
                     <div
                       key={index}
                       className={`h-full ${
@@ -39,25 +48,25 @@ const ButtonsDirections = () => {
                           : index === 6
                           ? "w-[6px] md:w-[10px] shadow-[inset_2px_0px_1px_#131313]"
                           : "w-[8px] md:w-[12px] shadow-[inset_2px_0px_1px_#131313,inset_-2px_0px_1px_#3a3a3a]"
-                     } bg-[#222]`}
+                      } bg-[#222]`}
                     ></div>
                   ))}
-                </div>
+              </div>
             </div>
-
+           
             <motion.div
-              className="col-span-1 rounded-[4px] md:rounded-md bg-[#d4d4cf] z-2 flex items-center justify-center 
-              text-lg md:text-2xl text-[#4a4840] cursor-pointer"
-              onClick={handleClick}
+              className="col-span-1 rounded-[4px] md:rounded-md bg-[#222] z-2 flex items-center justify-center 
+              text-lg md:text-2xl text-[#d4d4ce] cursor-pointer"
+              onClick={handleDownClick}
               animate={{
-                boxShadow: extraPressed
-                  ? "inset 2px 2px 5px #222, inset -2px -2px 5px #222"
-                  : "inset 2px 2px 1px #fff, inset -2px -2px 1px #979797, 5px 5px 8px #000",
+                boxShadow: downPressed
+                  ? "inset 2px 2px 5px #111, inset -2px -2px 5px #111"
+                  : "inset 2px 2px 1px #3a3a3a, inset -2px -2px 1px #131313, 5px 5px 8px #000",
               }}
               transition={{ duration: 0.1, ease: "easeInOut" }}
             >
               <ion-icon name="caret-down-outline"></ion-icon>
-              </motion.div>
+            </motion.div>
           </div>
         </div>
       </div>
@@ -66,3 +75,4 @@ const ButtonsDirections = () => {
 };
 
 export default ButtonsDirections;
+

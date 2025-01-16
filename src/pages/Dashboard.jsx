84d6a9262947974c9481e4014fb8c4cb8808ede9
alice bergonhe <alicebergonhe@gmail.com>
2025-01-16 11:10:36 +0100

    refactor: update import name for SoundBar component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -10,7 +10,7 @@ import Lights from "../components/Lights";
 import LightsPower from "../components/LightsPower";
 import EnterButton from "../components/EnterButton";
 import MenuButton from "../components/MenuButton";
-import Sound from "../components/Sound";
+import SoundBar from "../components/SoundBar";
 import PowerButton from "../components/PowerButton";
 import LoadingBar from "../components/LoadingBar";
 
@@ -124,7 +124,7 @@ const Dashboard = () => {
             </motion.div>
           </motion.div>
 
-          <Sound />
+          <SoundBar />
           <PowerButton
             handlePowerPress={handlePowerPress}
             isPowerOn={isPowerOn}

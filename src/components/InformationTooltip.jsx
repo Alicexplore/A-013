24b75c0d1eb927d0a9f7a55d526e7598eb7835a8
alice bergonhe <alicebergonhe@gmail.com>
2025-01-20 15:49:28 +0100

    style: update tooltip text and position

--- src/components/InformationTooltip.jsx
+++ src/components/InformationTooltip.jsx
@@ -1,17 +1,16 @@
 const InformationTooltip = () => {
   return (
-<div className="absolute top-5 right-5 md:top-10 md:right-10 flex flex-col items-end font-Roboto 
-    text-[#222] z-40 cursor-pointer group">
-    <span className="text-[12px] md:text-lg -mt-[2px] md:-mt-2">
-        <ion-icon name="information-circle"></ion-icon>
-    </span>
-    <div className="opacity-0 group-hover:opacity-100 w-[50%] md:w-1/3 bg-[#222] rounded-sm mt-2 transition-opacity duration-1000 
-        ">
-        <p className="m-[9px] md:m-4 text-[#d9d9d9] text-[7px] md:text-[11px] uppercase">
-            A-013 evolves continuously, allowing me to grow alongside it, 
-            meaning the A-013 you discover today may not be the same tomorrow. 
+<div className="absolute top-5 right-5 md:top-10 md:right-10 flex flex-row items-center font-Roboto 
+    text-[#222] z-40 cursor-pointer group gap-3">
+    <div className="opacity-0 group-hover:opacity-100 py-2 md:py-3 px-3 md:px-4 bg-[#222] rounded-sm transition-opacity 
+    duration-1000 ease-in-out">
+        <p className="text-[#d9d9d9] text-[7px] md:text-[11px] uppercase">
+          press power button
         </p>
     </div>
+    <span className="text-[12px] md:text-lg ">
+        <ion-icon name="information-circle"></ion-icon>
+    </span>
 </div>
 
 

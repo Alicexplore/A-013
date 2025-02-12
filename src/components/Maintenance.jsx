c5443a87251027f1c601fa99d7afcbec9bb50de7
alice bergonhe <alicebergonhe@gmail.com>
2025-02-12 09:47:45 +0100

    style: update release date on maintenance component

--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -62,7 +62,7 @@ const Maintenance = () => {
               viewport={{ once: true, amount: 0 }}
               className="inline font-bold"
             >
-              18th of February.
+              25th of February.
             </motion.p>
 
             <motion.p

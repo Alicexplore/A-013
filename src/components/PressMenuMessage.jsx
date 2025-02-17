c33243a3f69307f99c990e88a838122705cb7948
alice bergonhe <alicebergonhe@gmail.com>
2025-02-17 12:42:17 +0100

    style: update PressMenuMessage to display the menu

--- src/components/PressMenuMessage.jsx
+++ src/components/PressMenuMessage.jsx
@@ -1,31 +1,42 @@
-import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import ScrambleTextScreen from "./ScrambleTextScreen";
+import Menu from "./Menu";
 
 const PressMenuMessage = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowMenu(true);
+    }, 4000); 
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div>
-      <motion.div
-        className="absolute inset-0 flex flex-col items-center justify-center"
-        key="opening-screen"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ opacity: { ease: "linear", delay: 0 } }}>
-            
-        <ScrambleTextScreen
-          text="HELLO MY NAME IS A-013"
-          scrambleDuration={2.5}/>
-
-        <motion.div
-          className="absolute inset-0 flex items-center justify-center"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: [0, 1, 0] }}
-          transition={{ delay: 3.5, duration: 1.5, repeat: Infinity }}>
-          <h1 className="mt-[60px] sm:mt-[80px] text-[#fff] font-Roboto text-[9px] md:text-xs tracking-widest 
-          pointer-events-none uppercase">press menu</h1>
-        </motion.div>
-
-      </motion.div>
+    <div className="absolute inset-0 flex items-center justify-center">
+      <AnimatePresence mode="wait">
+        {!showMenu ? (
+          <motion.div
+            key="scramble-text"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }}>
+            <ScrambleTextScreen text="HELLO MY NAME IS A-013" scrambleDuration={2.5} />
+          </motion.div>
+        ) : (
+          <motion.div
+            className="w-full h-full"
+            key="new-component"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}>
+            <Menu />
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   );
 };

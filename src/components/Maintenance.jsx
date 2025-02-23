c81916963e04707d6ae43aa0c17e01cbc492eff2
alice bergonhe <alicebergonhe@gmail.com>
2025-02-23 20:57:17 +0100

    style: update maintenance screen text

--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -62,7 +62,7 @@ const Maintenance = () => {
               viewport={{ once: true, amount: 0 }}
               className="inline font-bold"
             >
-              4th of March.
+              4th of March ↗
             </motion.p>
 
             <motion.p

02d2716fb5f4e7225419547c993170f6ca55a95d
alice bergonhe <alicebergonhe@gmail.com>
2025-03-05 10:22:56 +0100

    feat: update menu page with currentComponent logic

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,9 +1,25 @@
-const Menu = () => {
-  return (
-    <div className="absolute w-full h-full items-center justify-center">
-        <h1>Menu</h1>
-    </div>
-  )
-}
+import PropTypes from "prop-types";
+import About from "../pages/About";
+import Projects from "../pages/Projects";
+import Stack from "../pages/Stack";
+import Contact from "../pages/Contact";
+import Instructions from "../pages/Instructions";
 
-export default Menu
+const Menu = ({ currentComponent }) => {
+  return (
+    <div className="absolute flex w-full h-full items-center justify-center">
+      {currentComponent === "instructions" && <Instructions />}
+      {currentComponent === "about" && <About />}
+      {currentComponent === "projects" && <Projects />}
+      {currentComponent === "stack" && <Stack />}
+      {currentComponent === "contact" && <Contact />}
+    </div>
+  );
+};
+
+Menu.propTypes = {
+  currentComponent: PropTypes.string.isRequired, 
+  setCurrentComponent: PropTypes.func.isRequired, 
+};
+
+export default Menu;

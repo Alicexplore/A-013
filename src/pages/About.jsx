5e9d275a705177c22991f8847c7b35318223162f
alice bergonhe <alicebergonhe@gmail.com>
2025-03-05 15:38:04 +0100

    style: update text in About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,25 +1,52 @@
-const About = () => {
-  return (
-    <div className="flex flex-col items-center  w-full h-full text-white uppercase p-10">
-      <h1 className="text-xs md:text-sm uppercase mt-2">
-        {/* <span className="mt-2 relative top-[2px]">
-          <ion-icon name="caret-forward-outline"></ion-icon>
-        </span>  */}
-        about
-      </h1>
-      <p className="text-[8px] md:text-[11px] mt-8 md:mt-12 text-justify">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      Eius vero commodi optio suscipit eos corrupti esse, non laudantium earum blanditiis consectetur reiciendis
-       molestias consequatur est. Libero ipsum repellendus autem possimus repellat eius soluta perferendis velit 
-       tenetur? Fugiat quaerat rem magni.
-       Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      Eius vero commodi optio suscipit eos corrupti esse, non laudantium earum blanditiis consectetur reiciendis
-       molestias consequatur est. Libero ipsum repellendus autem possimus repellat eius soluta perferendis velit 
-       tenetur? Fugiat quaerat rem magni.Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-       Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      </p>
-    </div>
-  )
-}
+import { motion } from 'framer-motion';
+import { fadeIn } from '../fadeIn';
 
-export default About
+const About = () => {
+    return (
+      <div className="flex flex-col items-center w-full h-full text-white uppercase p-[30px] md:p-10">
+        <motion.h1
+          className="text-[10px] md:text-sm font-roboto mt-1 md:mt-2"
+          initial="hidden"
+          whileInView="show"
+          variants={fadeIn(0.5)}
+          viewport={{ once: true, amount: 0 }}
+        > 
+          about
+        </motion.h1>
+            <div className="space-y-3 md:space-y-6">
+                <motion.p
+                  className="text-[7px] md:text-[11px] mt-8 md:mt-12 text-justify"
+                  initial="hidden"
+                  whileInView="show"
+                  variants={fadeIn(1)}
+                  viewport={{ once: true, amount: 0 }}
+                > 
+                  Hi, I&rsquo;m Alice Bergonhe, I&rsquo;m a frontend developer, 
+                  I&rsquo;ve been coding since 2022 and haven&rsquo;t looked back since.
+                 </motion.p>
+                 <motion.p
+                  className="text-[7px] md:text-[11px] mt-8 md:mt-12 text-justify"
+                  initial="hidden"
+                  whileInView="show"
+                  variants={fadeIn(1.5)}
+                  viewport={{ once: true, amount: 0 }}
+                > 
+                  I focus a lot on user experience, as I believe it’s essential to engage users and keep them hooked. 
+                  I also strive every day to write clean, performant code, and that’s an area I’m eager to keep improving.
+                 </motion.p>
+                 <motion.p
+                  className="text-[7px] md:text-[11px] mt-8 md:mt-12 text-justify"
+                  initial="hidden"
+                  whileInView="show"
+                  variants={fadeIn(2)}
+                  viewport={{ once: true, amount: 0 }}
+                > 
+                  After a year as a frontend engineer apprentice at Qonto, I’m now looking for a frontend developer role in a company 
+                  that values my work and helps me grow. If this resonates with you, feel free to reach out! ツ
+                 </motion.p>
+            </div>
+      </div>
+    )
+  }
+  
+  export default About

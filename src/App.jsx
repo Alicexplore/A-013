6ee231be4bc325ed78824fe6f00dcf2cee13706f
alice bergonhe <alicebergonhe@gmail.com>
2025-03-07 18:05:35 +0100

    refactor: delete Maintenance component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,9 @@
-import Maintenance from "./components/Maintenance";
 import Dashboard from "./pages/Dashboard"; 
 
 function App() {
-  const isMaintenance = true;
-
   return (
-    <div className="relative">
-  
-      <div className="relative z-0">
+    <div>
         <Dashboard />
-      </div>
-
-      {isMaintenance && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center">
-          <Maintenance />
-        </div>
-      )}
     </div>
   );
 }

cc6f72e18302dd2158abda0c7ddbc5c353454398
alice bergonhe <alicebergonhe@gmail.com>
2025-03-07 17:23:44 +0100

    feat: update Contact page with personal informations

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,25 +1,57 @@
+import { motion } from 'framer-motion';
+import ScrambleText from "../components/ScrambleText"
+
 const Contact = () => {
   return (
-    <div className="flex flex-col items-center  w-full h-full text-white uppercase p-10">
-      <h1 className="text-xs md:text-sm uppercase mt-2">
-        {/* <span className="mt-2 relative top-[2px]">
-          <ion-icon name="caret-forward-outline"></ion-icon>
-        </span>  */}
-        contact
-      </h1>
-      <p className="text-[8px] md:text-[11px] mt-8 md:mt-12 text-justify">
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      Eius vero commodi optio suscipit eos corrupti esse, non laudantium earum blanditiis consectetur reiciendis
-       molestias consequatur est. Libero ipsum repellendus autem possimus repellat eius soluta perferendis velit 
-       tenetur? Fugiat quaerat rem magni.
-       Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      Eius vero commodi optio suscipit eos corrupti esse, non laudantium earum blanditiis consectetur reiciendis
-       molestias consequatur est. Libero ipsum repellendus autem possimus repellat eius soluta perferendis velit 
-       tenetur? Fugiat quaerat rem magni.Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-       Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-      </p>
-    </div>
-  )
-}
+    <motion.div
+      className="flex flex-col w-full h-full uppercase p-[32px] md:p-12 overflow-hidden z-50 text-white font-Roboto cursor-default relative"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1, ease: 'easeInOut' }}
+      viewport={{ once: true, amount: 0 }}
+    >
+      <div className="flex items-center justify-between w-full text-[10px] md:text-sm text-center font-medium">
+        <span className="flex-1 h-[1px] bg-white"></span>
+        <h1 className="px-4">contact</h1>
+        <span className="flex-1 h-[1px] bg-white"></span>
+      </div>
 
-export default Contact
+      <div className="flex-1 mt-6 md:mt-8 overflow-y-auto text-[9px] md:text-[12px] space-y-6 md:space-y-8 font-normal">
+        <div className="leading-relaxed flex flex-col">
+          <h2>alice bergonhe</h2>
+          <p>paris</p>
+        </div>
+        <div className="leading-relaxed flex flex-col">
+          <a href="mailto:alicebergonhe@gmail.com">
+            <span className="inline-block">
+              <ScrambleText
+                text="alicebergonhe@gmail.com"
+                scrambleDuration={2.5}
+              />
+            </span>
+          </a>
+        </div>
+        <div className="leading-relaxed flex flex-col">
+          <a href="https://www.linkedin.com/in/alicebergonhe/" target="_blank">
+            <span className="inline-block">
+              <ScrambleText
+                text="linkedin"
+                scrambleDuration={1.5}
+              />
+            </span>
+          </a>
+          <a href="https://github.com/Alicexplore" target="_blank">
+            <span className="inline-block">
+              <ScrambleText
+                text="github"
+                scrambleDuration={1.3}
+              />
+            </span>
+          </a>
+        </div>
+      </div>
+    </motion.div>
+  );
+};
+
+export default Contact;

9489e5d4aabf4f1e273952b9dd25e6bffe495f6e
alice bergonhe <alicebergonhe@gmail.com>
2025-03-08 09:12:41 +0100

    style: push Maintenance component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import Maintenance from "./components/Maintenance";
 import Dashboard from "./pages/Dashboard"; 
 
 function App() {
+  const isMaintenance = true;
+
   return (
-    <div>
+    <div className="relative">
+  
+      <div className="relative z-0">
         <Dashboard />
+      </div>
+
+      {isMaintenance && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          <Maintenance />
+        </div>
+      )}
     </div>
   );
 }

382f35b72f07d59488c1da5cb245d9a770dac076
alice bergonhe <alicebergonhe@gmail.com>
2026-05-30 17:16:24 +0200

    refactor: update versioning

--- src/components/TextFolio.jsx
+++ src/components/TextFolio.jsx
@@ -2,7 +2,7 @@ const TextFolio = () => {
   return (
     <div className="absolute bottom-5 right-5 md:bottom-5 md:right-10 font-Roboto text-[9px] md:text-xs tracking-widest 
       text-[#222] mb-0 sm:mb-5 sm:text-left pointer-events-none">
-      <h1>A-013 / V1.0.0</h1>
+      <h1>A-013 / V1.1.0</h1>
         <div className="flex items-baseline uppercase">
           <h1>developed in 2025</h1>
         </div>
